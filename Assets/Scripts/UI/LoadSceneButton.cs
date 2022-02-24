65ce306a12e96b52b85ddfcd912e45613a641d38
BeeepBeepSheep <jacoblueke@gmail.com>
2022-02-24 17:19:24 +0000

    hub and menus polished, final build version

--- Assets/Scripts/UI/LoadSceneButton.cs
+++ Assets/Scripts/UI/LoadSceneButton.cs
@@ -7,12 +7,19 @@ namespace Unity.LEGO.UI
     public class LoadSceneButton : MonoBehaviour
     {
         public string sceneName = "";
-
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
         public void LoadScene()
         {
             SceneManager.LoadScene(sceneName);
         }
-
+        public void LoadHub()
+        {
+            SceneManager.LoadScene(1);
+        }
         public void LoadPreviousScene()
         {
             if (GameFlowManager.PreviousScene != null)
